dd77fed1c05d87abc48beea5be180db719664679
Adithya Shetty <adithshetty@deloitte.com>
2025-08-07 17:16:22 +0000

    init: initialised dashboard application

---
+++ app/dashboard/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("aifwrapper.app.dashboard.dashboard.controller.View1", {
+        onInit() {
+        }
+    });
+});
