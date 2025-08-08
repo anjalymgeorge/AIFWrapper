e42df026250d3a59dcc4016b169c44711d053653
Adithya Shetty <adithshetty@deloitte.com>
2025-08-08 14:23:06 +0000

    patch: re-adding handlers

---
+++ srv/handlers.js
@@ -0,0 +1,128 @@
+const { Util } = require('./utils/common');
+
+const { HTTP_METHODS, HTTP_STATUS } = require('./core/enums');
+const { Connectivity } = require('./core/connectivity');
+
+
+class Handlers {
+
+    static async triggerWorkflowHandler(req, next) {
+
+        const data = await Handlers._createWorkflowTasks(req);
+
+    }
+
+    /**
+     * Triggers the workflow with the passed context.
+     */
+    static async _triggerWorkflow(context, req) {
+        const endpoint = "/v1/workflow-instances"
+        /**The workflow destinaiton name string. */
+        const workflowDestination = "ProcessAutomation"
+
+        const wfConnectivity = Connectivity.for(workflowDestination);
+        wfConnectivity.registerClientRequest(req).setUserTokenFromRequest();
+
+        return new Promise(async (res, rej) => {
+            const response = await wfConnectivity.request(endpoint, HTTP_METHODS.POST, context);
+
+            if (response.status !== HTTP_STATUS.CREATED) {
+                rej(response);
+            }
+
+            res(response);
+        });
+    }
+
+    //TODO: move this to a sperate worflow handler module.
+
+    static async _createWorkflowTasks(req) {
+        //TODO: add this to env variables.
+        const definationId = "eu10.btp-innovate-yjy2sr7n.cfinworlflow.cFINProcess";
+        const worflowExecutions = [];
+        const errorsForTasksToBeCreated = await Handlers._getErrorsForPreviousHour(req) ?? [];
+
+        //TODO: need to implement a check function that will check if an instance of wf is running for the current error
+        errorsForTasksToBeCreated.slice(0,2).forEach((data) => {
+            const context = {
+                "definitionId": definationId,
+                "context": {
+                    "errordetails": {
+                        //NOTE: Hardcoded 
+                        "errCatGroup": "Mapping",
+                        "errCat": data?.ErrorCategory,
+                        //NOTE: SYS id is hardcoded  
+                        "logicalSystemid": "DGM",
+                        "targetCompanyCode": "",
+                        "messageNumber": data?.MsgNumber,
+                        "responsibleTeam": "",
+                        "responsibleTeamName": "",
+                        "status": "",
+                        "interfaceName": data?.InterfaceName,
+                        "messageClass": data?.MsgClass,
+                        "transactionId": "",
+                        "messageText": data?.FinalText,
+                        "MsgNumber": "",
+                        "msgGuid": data?.msgGuid,
+                        "interfaceVer": data?.InterfaceVer,
+                        "numberOfErrors": data?.NumberOfErrors,
+                        "numberOfWarnings": data?.NumberOfWarnings,
+                        "numberOfSuccess": data?.NumberOfSuccess,
+                        "lastUser": data?.LastUser,
+                        "lastDate": Util.getISODateStringFromOdataDate(data?.LastDate),
+                        "createUser": data?.CreateUser,
+                        "createDate": Util.getISODateStringFromOdataDate(data?.CreateDate)
+                    }
+                }
+            }
+
+            worflowExecutions.push(Handlers._triggerWorkflow(context, req));
+        });
+
+        const executionResults = await Promise.allSettled(worflowExecutions);
+
+        return executionResults;
+    }
+
+    /**
+     * Gets the Error data for the time block of an hour before from current time.
+     * @param {} req The client request object
+     * @returns {Array.<{}>} The array of error.
+     * @throws {Error} 
+     * @private
+     * @static
+     */
+    static async _getErrorsForPreviousHour(req) {
+        try {
+            const dgnDestination = "DGN_AIF";
+            /**@type {Date} the current datetime */
+            const now = new Date();
+            /**@type {Date} the datetime an hour earlier */
+            const hourBefore = new Date(new Date().setHours(now.getHours() - 1));
+
+            /**The url endpoint from the  */
+            const endpoint = "/sap/opu/odata/sap/ZAIF_ERRORS_CDS/zaif_errors"
+
+
+            const filterParam = `CreateDate le datetime'${Util.getOdataDateTimeString(now)}' and CreateDate ge datetime'${Util.getOdataDateTimeString(hourBefore)}'`
+
+            const dgnConnectivity = Connectivity.for(dgnDestination);
+            dgnConnectivity.registerClientRequest(req).setUserTokenFromRequest();
+
+            // Do a http call. 
+            const response = await dgnConnectivity.request(`${endpoint}`, HTTP_METHODS.GET, undefined);
+            if (response.status !== HTTP_STATUS.OK) {
+                throw new Error(`HTTP request to backend failed due to- ${response.status | response?.status_message}. Error: ${JSON.stringify(response?.body)}`);
+            }
+
+            return response?.body?.d?.results ?? [];
+        } catch (error) {
+            console.error(`[ERROR] Unable to get data from backend.`, error);
+            // propagate the error.
+            throw error;
+        }
+    }
+}
+
+
+module.exports = { Handlers }
