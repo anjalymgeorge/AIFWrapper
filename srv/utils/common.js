81c280ddce6ec17a2a8d0a9c2978f02cb3564a80
Adithya Shetty <adithshetty@deloitte.com>
2025-08-09 08:14:29 +0000

    patch: added invertedDate function and converted string to number.

--- srv/utils/common.js
+++ srv/utils/common.js
@@ -6,6 +6,25 @@
  * @public
  */
 class Util {
+
+    /**
+     * Inverts the date by doing a 9's complement.
+     *
+     * A method used by sap to sort dates in few legacy and current tables, so that the latest date is always a higher number for processing.
+     * This inversion will be Used for getting filtered values from odata which support invereted date format as a date filter.
+     * Will remove addtion of addtional lines to the ODATA Function modules to convert and return data for the reuired date.
+     *
+     * **Note: The technique of manipulating the sort sequence of dates by inverting the internal date format is now rarely used.**
+     *
+     * @param {Date} date date object to be converted.
+     * @returns {Number} passed date in inverted date format in a decimal format.
+     */
+    static invertedDate(date) {
+        //converts the date to decimal format of yyyymmdd format, a SAP orginal format.
+        const _date = date.getUTCFullYear() * 10000 + (date.getUTCMonth() + 1) * 100 + date.getUTCDate();
+        return 99999999 - _date;
+    }
+
     /**
      * Converts a given date object to its odata datetime representation string.
      * @param {Date} date - The date object.
@@ -25,8 +44,8 @@ class Util {
      * @public
      */
     static getISODateStringFromOdataDate(dateString) {
-        const datetimeString = dateString.replace(/([\/]|Date|[\(\)])/g, '');
-        const date = new Date(datetimeString);
+        const datetime = new Number(dateString.replace(/([\/]|Date|[\(\)])/g, ''));
+        const date = new Date(datetime);
         return date.toISOString();
     }
 }
